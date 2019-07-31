fc2b15dd2b074871e12c72d63636feeeb4d557a4
shail <sds38@njit.edu>
2019-07-31 03:19:29 -0400

    updated setup for thank you page

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,20 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function()
+{
+    return View::make('pages.home');
+});
+Route::get('about', function()
+{
+    return View::make('pages.about');
+});
+Route::get('projects', function()
+{
+    return View::make('pages.projects');
+});
+Route::get('contact', function()
+{
+    return View::make('pages.contact');
 
-Route::get('/', function () {
-    return view('welcome');
 });
